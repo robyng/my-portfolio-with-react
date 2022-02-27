c72897ce24d7b60ae7fdaa6b16e82664c99140e3
Robyn Graham <robyngraham01@gmail.com>
2022-02-27 02:08:32 -0800

    fix home link in logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import logo from '../../assets/images/logo.png'
 function Header(props) {
     return(
         <header className="d-flex justify-content-between">
-          <a href='/' >
+          <a href='/my-portfolio-with-react/' >
 <h1><img src={logo} className="logo" height="50px" alt=''/>Robyn's Portfolio</h1></a>  
            {props.children}
         </header>
