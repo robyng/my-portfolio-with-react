115565cfb14bf994cac555fd9c6b458f49b74ea8
robyng <robyngraham01@gmail.com>
2022-07-18 16:56:50 -0700

    add new logos for laptop and email, update footer and contact page.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,49 +1,81 @@
 import React, { useState } from "react";
+import github from '../../assets/images/git-hub.png'
+import linkedin from '../../assets/images/linkedin.png'
+import laptop from '../../assets/images/laptop-code-solid-color-sm.png'
+import mail from '../../assets/images/envelope-solid-color.png'
 // import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
-//     const [errorMessage, setErrorMessage] = useState('');
-//     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-//     const { name, email, message } = formState;
+    //     const [errorMessage, setErrorMessage] = useState('');
+    //     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    //     const { name, email, message } = formState;
 
-//     function handleChange(e) {
-//         if (e.target.name === 'email') {
-//             const isValid = validateEmail(e.target.value);
-//             console.log(isValid)
-//             //isvalid conditional
-//             if (!isValid) {
-//                 setErrorMessage('Your email is invalid');
-//             } else {
-//                 setErrorMessage('')
-//             }
-//         } else {
-//             if (!e.target.value.length) {
-//                 setErrorMessage(`${e.target.name} is required.`);
-//             } else {
-//                 setErrorMessage('');
-//             }
-//         }
+    //     function handleChange(e) {
+    //         if (e.target.name === 'email') {
+    //             const isValid = validateEmail(e.target.value);
+    //             console.log(isValid)
+    //             //isvalid conditional
+    //             if (!isValid) {
+    //                 setErrorMessage('Your email is invalid');
+    //             } else {
+    //                 setErrorMessage('')
+    //             }
+    //         } else {
+    //             if (!e.target.value.length) {
+    //                 setErrorMessage(`${e.target.name} is required.`);
+    //             } else {
+    //                 setErrorMessage('');
+    //             }
+    //         }
 
 
-//         console.log('errorMessage', errorMessage);
+    //         console.log('errorMessage', errorMessage);
 
-//         if (!errorMessage) {
-//             setFormState({ ...formState, [e.target.name]: e.target.value })
+    //         if (!errorMessage) {
+    //             setFormState({ ...formState, [e.target.name]: e.target.value })
 
-//         }
+    //         }
 
-//     }
+    //     }
 
-//     function handleSubmit(e) {
-//         e.preventDefault();
-//         console.log(formState);
-//     }
+    //     function handleSubmit(e) {
+    //         e.preventDefault();
+    //         console.log(formState);
+    //     }
 
     return (
         <section class="container form-div col-lg-6 col-md-10">
-        
+
             <h2>Contact me</h2>
-            <a href='mailto:robyn@sitereworks.com' target='_blank' rel='noreferrer'>robyn@sitereworks.com</a>
+            <div class="contact">
+                <div class="rmg-block">
+                    <a href='mailto:robyn@sitereworks.com' target='_blank' rel='noreferrer'><img src={mail} class="icon" height="100px" alt='' /></a>
+                    <br />
+
+                    <a href='mailto:robyn@sitereworks.com' target='_blank' rel='noreferrer'>robyn@sitereworks.com</a>
+                </div>
+
+                <br />
+                <div class="rmg-block">
+                    <a href="https://github.com/robyng" target='_blank' rel="noreferrer" ><img src={github} class="icon" height="100px" alt='' /></a>
+                    <br />
+                    Github
+                </div>
+                <br />
+                <div class="rmg-block">
+                    <a href="https://www.linkedin.com/in/robyn-graham-b067aa97/" target='_blank' rel="noreferrer"><img src={linkedin} class="icon" height="100px" alt='' /></a>
+                    <br />
+                    Linkedin
+                </div>
+                <br />
+                <div className="rmg-block">
+                    <a href="https://www.sitereworks.com/" target='_blank' rel="noreferrer"><img src={laptop} class="icon" target='_blank' height="100px" alt='' /></a>
+                    <br />
+                    Consulting
+                </div>
+            </div>
+
+
             {/* <form id="contact-form" onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="name">Name:</label> <br />
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import github from '../../assets/images/git-hub.png'
 import linkedin from '../../assets/images/linkedin.png'
-import laptop from '../../assets/images/laptop-icon-circle.png'
-
+import laptop from '../../assets/images/laptop-code-solid-color-sm.png'
+import mail from '../../assets/images/envelope-solid-color.png'
 function Footer() {
     return(
         <footer>
-<a href="https://github.com/robyng"><img src={github} class="icon" height="25px" alt='' /></a>
-<a href="https://www.linkedin.com/in/robyn-graham-b067aa97/"><img src={linkedin} class="icon" height="25px" alt='' /></a>
-<a href="https://www.sitereworks.com/"><img src={laptop} class="icon" height="25px" alt='' /></a>
+              <a href='mailto:robyn@sitereworks.com' target='_blank' rel='noreferrer'><img src={mail} className="icon" height="25px" alt='Email' /></a>
+<a href="https://github.com/robyng"><img src={github} className="icon" height="25px" alt='' /></a>
+<a href="https://www.linkedin.com/in/robyn-graham-b067aa97/"><img src={linkedin} className="icon" height="25px" alt='' /></a>
+<a href="https://www.sitereworks.com/"><img src={laptop} className="icon" height="25px" alt='' /></a>
 <br/>
            <p>&copy; {(new Date().getFullYear())} Robyn Graham</p>
         </footer>
